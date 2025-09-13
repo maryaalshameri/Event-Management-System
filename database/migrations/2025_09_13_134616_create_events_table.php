3830e2b5695b9a96202a756fabf7e763ba46db96
maryaalshameri <marynaalshameri@gmail.com>
2025-09-13 17:11:27 +0300

    Feat: Add fields to events table

--- database/migrations/2025_09_13_134616_create_events_table.php
+++ database/migrations/2025_09_13_134616_create_events_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('organizer_id')->constrained('users');
+            $table->string('title');
+            $table->text('description');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->string('location');
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->integer('capacity')->default(0);
+            $table->string('category');
+            $table->integer('available_seats');
+            $table->decimal('price', 8, 2)->default(0);
+            $table->string('image')->nullable();
+            $table->enum('status', ['draft', 'published', 'cancelled', 'completed'])->default('draft');
             $table->timestamps();
         });
     }
