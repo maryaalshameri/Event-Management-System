84a01cf8576546c19a47dbb636731bc09713bbc9
maryaalshameri <marynaalshameri@gmail.com>
2025-09-13 18:13:08 +0300

    Feat: Add fields to orders table

--- database/migrations/2025_09_13_143242_create_orders_table.php
+++ database/migrations/2025_09_13_143242_create_orders_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+            $table->enum('status', ['reserved', 'confirmed', 'cancelled'])->default('reserved');
+            $table->decimal('total_amount', 10, 2)->default(0);
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
